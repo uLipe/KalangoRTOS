3caf5af33d48f45e659e60def6b258e2dec837bd
Felipe Neves <ryukokki.felipe@gmail.com>
2026-07-16 23:33:31 -0300

    arch/tricore: keep board details out of startup.S
    - Backport from main for 0.1.0

--- cmake/arch_sources.cmake
+++ cmake/arch_sources.cmake
@@ -3,14 +3,16 @@
 if(ULMK_ARCH STREQUAL "tricore")
 	set(ULMK_ARCH_KERNEL_SOURCES
 		${ULMK_ARCH_DIR}/arch.c
+		${ULMK_ARCH_DIR}/smp.c
 		${ULMK_ARCH_DIR}/ctx_switch.S)
 	set(ULMK_ARCH_EXE_SOURCES
 		${ULMK_ARCH_DIR}/startup.S
-		${ULMK_ARCH_DIR}/board_wdt_early_stub.S
+		${ULMK_ARCH_DIR}/secondary.S
 		${ULMK_ARCH_DIR}/vectors.S)
 elseif(ULMK_ARCH STREQUAL "riscv")
 	set(ULMK_ARCH_KERNEL_SOURCES
 		${ULMK_ARCH_DIR}/arch.c
+		${ULMK_ARCH_DIR}/smp.c
 		${ULMK_ARCH_DIR}/irq.c
 		${ULMK_ARCH_DIR}/irq_clint.c
 		${ULMK_ARCH_DIR}/irq_clic.c
@@ -29,7 +31,6 @@ elseif(ULMK_ARCH STREQUAL "arm")
 		${ULMK_ARCH_DIR}/trap.S)
 	set(ULMK_ARCH_EXE_SOURCES
 		${ULMK_ARCH_DIR}/startup.S
-		${ULMK_ARCH_DIR}/board_wdt_early_stub.S
 		${ULMK_ARCH_DIR}/vectors.S)
 else()
 	message(FATAL_ERROR "Unsupported ULMK_ARCH=${ULMK_ARCH}")
--- tests/arch/tricore.mk
+++ tests/arch/tricore.mk
@@ -17,8 +17,8 @@ ARCH_LDFLAGS := -mcpu=tc39xx
 
 ARCH_KERNEL_SRCS := \
 	$(ROOT)/arch/tricore/startup.S \
-	$(ROOT)/arch/tricore/board_wdt_early_stub.S \
 	$(ROOT)/arch/tricore/arch.c \
+	$(ROOT)/arch/tricore/smp.c \
 	$(ROOT)/arch/tricore/ctx_switch.S \
 	$(ROOT)/arch/tricore/vectors.S \
 	$(ROOT)/boards/qemu_tc3xx/qemu_printk_hook.c
